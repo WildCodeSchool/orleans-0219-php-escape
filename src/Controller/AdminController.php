73409c2c1b6db01029cf83a4336e67594de4fca2
MaistrePedro <peterpendrag@gmail.com>
2019-04-26 15:20:46 +0200

    admin home finished

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use App\Model\ItemManager;
+use App\Model\MissionsManager;
+
 class AdminController extends AbstractController
 {
 
@@ -17,6 +20,31 @@ class AdminController extends AbstractController
      */
     public function admin()
     {
-        return $this->twig->render('Admin/admin.html.twig');
+        $missionsManager = new MissionsManager();
+        $missions = $missionsManager->selectAll();
+
+        return $this->twig->render('Admin/admin.html.twig', ['missions' => $missions]);
+    }
+
+    /**
+     * Display item edition page specified by $id
+     *
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function edit(int $id): string
+    {
+        $missionsManager = new MissionsManager();
+        $missions = $missionsManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $mission['title'] = $_POST['title'];
+            $missionsManager->update($missions);
+        }
+
+        return $this->twig->render('Item/edit.html.twig', ['missions' => $item]);
     }
 }
---
+++ src/Controller/MissionsController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\MissionsManager;
+
+class MissionsController extends AbstractController
+{
+
+}
