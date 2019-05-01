62f07beca0c5f369118e1805330797a64cb6c123
Shirley Bismuth <shirley.bsth@gmail.com>
2019-05-01 14:23:37 +0200

    ajout de la partie Add côté Admin, ajout en BDD et création d'une card mission dans la partie Mission/index

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -53,4 +53,25 @@ class AdminController extends AbstractController
 
         return $this->twig->render('Admin/edit.html.twig', ['mission' => $mission]);
     }
+
+    public function add()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $missionManager = new missionManager();
+            $mission = [
+                'title' => $_POST['title'],
+                'minplayers' => $_POST['minplayers'],
+                'maxplayers' => $_POST['maxplayers'],
+                'level' => $_POST['level'],
+                'subtitle' => $_POST['subtitle'],
+                'description' => $_POST['description'],
+                'image' => $_POST['image'],
+            ];
+            $missionManager->insert($mission);
+            header('Location:/admin/index');
+        }
+
+        return $this->twig->render('/Admin/add.html.twig');
+    }
 }
